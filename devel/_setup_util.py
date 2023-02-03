0983a82f6971b082457bf37a2f118a17827d9547
francomano <playerhorus@gmail.com>
2023-02-03 13:21:29 +0100

    first commit

---
+++ devel/_setup_util.py
@@ -0,0 +1,1 @@
+/home/mf/Scrivania/2d_normal_lidar_odom/devel/.private/catkin_tools_prebuild/_setup_util.py
---
+++ devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/mf/Scrivania/2d_normal_lidar_odom/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
